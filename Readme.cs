30a3744aaf241d243fef4ccd9ed01a28d9e3dca8
hamzafoy <hamzaoflouisville@gmail.com>
2022-01-30 18:33:47 -0500

    Add project files.

---
+++ Readme.cs
@@ -0,0 +1,6 @@
+﻿/*
+ * 
+ * This repository is composed of multiple subdirectories storing miscellaneous, smaller codebases
+ * that have been developed as I become more familiar with the C# language.
+ * 
+ */
