2a1e46f045d34bebd7350492a6f65c0255638da2
hamzafoy <hamzaoflouisville@gmail.com>
2022-02-11 07:23:57 -0500

    Moved the Main Method to a Console App file in the primary directory of this repository.

--- ArchitecturalApproximation/ApproximateOfArea.cs
+++ ArchitecturalApproximation/ApproximateOfArea.cs
@@ -1,15 +1,10 @@
 ﻿using System;
 
 
-namespace CSharpMiscellaneousProjects.ArchitecturalApproximation
+namespace CSharpMiscellaneousProjects_ArchitecturalApproximation
 {
-    class Program
+    class ArchitecturalCalculation
     {
-        public static void Main(string[] args)
-        {
-            double testApproximation = ApproximateArea(rLength: 150, rWidth: 75, cRadius: 25, tBase: 100, tHeight: 200);
-            Console.Write($"The approximate area of your house is: {Decimal.Round((decimal)testApproximation,2)} meters");
-        }
         public static double ApproximateArea(double rLength, double rWidth, double cRadius, double tBase, double tHeight)
         {
             return ((rLength * rWidth) + (Math.PI * Math.Pow(cRadius, 2)) + (0.5 * tBase * tHeight));
--- Readme.cs
+++ Readme.cs
@@ -4,3 +4,18 @@
  * that have been developed as I become more familiar with the C# language.
  * 
  */
+
+using System;
+using CSharpMiscellaneousProjects_ArchitecturalApproximation;
+
+namespace CSharpMiscellaneousProjects
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            double testApproximation = ArchitecturalCalculation.ApproximateArea(rLength: 150, rWidth: 75, cRadius: 25, tBase: 100, tHeight: 200);
+            Console.Write($"The approximate area of your house is: {Decimal.Round((decimal)testApproximation, 2)} meters");
+        }
+    }
+}
