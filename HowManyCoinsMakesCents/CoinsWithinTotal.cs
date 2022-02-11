8cf8723baa861d1fa0f004af0181fd1f2b662a39
hamzafoy <hamzaoflouisville@gmail.com>
2022-02-10 22:16:17 -0500

    Began to develop a new method that will help approximate area for an architectural build that is split up between rectangles, circles, and triangles. Will move Main into a separate file in the upper-level directory of CSharpMisc.

---
+++ HowManyCoinsMakesCents/CoinsWithinTotal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MoneyMaker
+{
+    class MainClass
+    {
+        //public static void Main(string[] args)
+        //{
+        //    int quarter = 25, dime = 10, nickel = 5;
+        //    Console.WriteLine("Interested in learning how many of each coin\nyou need to reach a certain amount?\nLook no further!");
+        //    Console.WriteLine("Please give an amount to convert: ");
+        //    string userAnswer = Console.ReadLine();
+        //    double answerAsADouble = Math.Floor(Convert.ToDouble(userAnswer));
+        //    Console.WriteLine($"{answerAsADouble} cents is equal to...");
+        //    double quarters = Math.Floor(answerAsADouble / quarter);
+        //    double remainingAmount = answerAsADouble % quarter;
+        //    double dimes = Math.Floor(remainingAmount / dime);
+        //    remainingAmount %= dime;
+        //    double nickels = Math.Floor(remainingAmount / nickel);
+        //    remainingAmount %= nickel;
+        //    Console.WriteLine($"Quarters: {quarters}\nDimes: {dimes}\nNickels: {nickels}\nPennies: {remainingAmount}");
+        //}
+    }
+}
