6b1e5443c00bd9c5a5a30cf57f99bf00dd42f1f5
hamzafoy <hamzaoflouisville@gmail.com>
2022-05-18 23:11:23 -0400

    Added small project based on 'Exercises for Programmers: 57 Challenges to Develop Your Coding Skills'

---
+++ HelloInManyLanguages/HelloMarhabaNihao.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpMiscellaneousProjects_HelloInManyLanguages
+{
+    public class HelloMarhabaNihao
+    {
+        public static string GreetMe()
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("What language do you speak?");
+            Console.WriteLine("AR for Arabic, EN for English, SO for Somali");
+            string language = Console.ReadLine();
+            switch (language)
+            {
+                case "AR":
+                    return $"Merhaban {name}, kayfa haluka?";
+                case "EN":
+                    return $"Hello {name}, how are you?";
+                case "SO":
+                    return $"Salam {name}, iska warran?";
+                default:
+                    return $"Dunno what language you speak mate!";
+            }
+        }
+    }
+}
