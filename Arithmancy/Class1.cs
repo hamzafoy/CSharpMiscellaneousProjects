a67f89734d2bdb654a0455d2910340f5126d74fd
hamzafoy <hamzaoflouisville@gmail.com>
2022-10-09 17:51:14 -0400

    Beginning arithmetic class

---
+++ Arithmancy/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpMiscellaneousProjects_Arithmancy
+{
+    public class ArithmeticOperations
+    {
+        public static class Arithmetics
+        {
+            public static void Addition(int firstvalue, int secondvalue)
+            {
+                Console.WriteLine($"{firstvalue} + {secondvalue} equals {firstvalue + secondvalue}");
+            }
+        }
+        public static class Remote
+        {
+            private static char selection = 'X';
+            public static char Selection
+            {
+                get { return selection; }
+                set { selection = Char.ToUpper(value); }
+            }
+
+
+        }
+    }
+}
